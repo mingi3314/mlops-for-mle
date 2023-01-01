d9ed6d88ee3e1fdeec11a9e822432f72486d7acc
Minki Kim <mingi3314@gmail.com>
2023-01-01 23:43:58 +0900

    refactor: set db host as argument

--- src/database/data_generator.py
+++ src/database/data_generator.py
@@ -1,14 +1,24 @@
 import time
 
+import click
+
 from src.database.create_table import create_table
 from src.database.engine import create_engine
 from src.database.insert_data import fetch_data, insert_data
 
-if __name__ == "__main__":
-    create_table(engine=create_engine())
+
+@click.option("--db-host", default="localhost", help="Database host")
+@click.command()
+def main(db_host: str) -> None:
+    engine = create_engine(db_host)
+    create_table(engine)
 
     df_iris = fetch_data()
 
     for data in df_iris.to_dicts():
-        insert_data(create_engine(), data)
+        insert_data(engine, data)
         time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
