7cffc0b150063c65fd06097c6ad8d23192eeb3ed
Minki Kim <mingi3314@gmail.com>
2023-01-01 23:33:08 +0900

    refactor: extract method

--- src/database/data_generator.py
+++ src/database/data_generator.py
@@ -1,14 +1,14 @@
 import time
 
 from src.database.create_table import create_table
-from src.database.engine import engine
+from src.database.engine import create_engine
 from src.database.insert_data import fetch_data, insert_data
 
 if __name__ == "__main__":
-    create_table(engine=engine)
+    create_table(engine=create_engine())
 
     df_iris = fetch_data()
 
     for data in df_iris.to_dicts():
-        insert_data(engine, data)
+        insert_data(create_engine(), data)
         time.sleep(1)
--- src/database/engine.py
+++ src/database/engine.py
@@ -1,5 +1,10 @@
 import sqlalchemy as sa
+from sqlalchemy.engine.base import Engine
 
 from src.config import DB_URL
 
-engine = sa.create_engine(DB_URL, echo=True)
+
+def create_engine(host: str = None) -> Engine:
+    if host:
+        DB_URL.replace("localhost", host)
+    return sa.create_engine(DB_URL, echo=True)
