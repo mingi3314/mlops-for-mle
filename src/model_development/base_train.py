81f2772b5054a33223b16cc30e0d962cacf844e5
Minki Kim <mingi3314@gmail.com>
2023-01-07 00:48:57 +0900

    feat: add base_train module

---
+++ src/model_development/base_train.py
@@ -0,0 +1,80 @@
+import os
+
+import joblib
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
+def load_data() -> pd.DataFrame:
+    df = load_iris(as_frame=True).frame
+    return df
+
+
+def preprocess(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    X = df.drop("target", axis=1)
+    y = df["target"]
+
+    # Split the data into train and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test
+
+
+def scale_data(X_train: pd.DataFrame, X_test: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    scaler = StandardScaler()
+    scaled_X_train = scaler.fit_transform(X_train)
+    scaled_X_test = scaler.transform(X_test)
+    return scaled_X_train, scaled_X_test
+
+
+def train(scaled_X_train: pd.DataFrame, y_train: pd.Series) -> SVC:
+    model = SVC()
+    model.fit(scaled_X_train, y_train)
+
+    y_train_hat = model.predict(scaled_X_train)
+
+    print("Train accuracy:", accuracy_score(y_train, y_train_hat))
+    return model
+
+
+def test(scaled_X_test: pd.DataFrame, y_test: pd.Series, model: SVC) -> None:
+    y_test_hat = model.predict(scaled_X_test)
+
+    print("Test accuracy:", accuracy_score(y_test, y_test_hat))
+
+
+def save_model(model: SVC, scaler: StandardScaler, directory: str) -> None:
+    os.makedirs(directory, exist_ok=True)
+    joblib.dump(model, f"{directory}/model.pkl")
+    joblib.dump(scaler, f"{directory}/scaler.pkl")
+
+
+def load_model(directory: str) -> tuple[SVC, StandardScaler]:
+    model = joblib.load(f"{directory}/model.pkl")
+    scaler = joblib.load(f"{directory}/scaler.pkl")
+    return model, scaler
+
+
+def main() -> None:
+    df = load_data()
+    X_train, X_test, y_train, y_test = preprocess(df)
+
+    model_dir = "src/model_development/models"
+    if os.path.exists("models/model.pkl"):
+        model, scaler = load_model(model_dir)
+    else:
+        scaler = StandardScaler()
+        scaled_X_train = scaler.fit_transform(X_train)
+        model = train(scaled_X_train, y_train)
+        save_model(model, scaler, model_dir)
+
+    scaled_X_test = scaler.transform(X_test)
+    test(scaled_X_test, y_test, model)
+
+
+if __name__ == "__main__":
+    main()
