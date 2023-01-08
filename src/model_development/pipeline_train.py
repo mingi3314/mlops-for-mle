02ee47b2334761a00c66e82610d32ef7c1932e51
Minki Kim <mingi3314@gmail.com>
2023-01-08 17:52:02 +0900

    refactor: extract main method

--- src/model_development/pipeline_train.py
+++ src/model_development/pipeline_train.py
@@ -4,16 +4,22 @@ from sklearn.svm import SVC
 
 from src.model_development.base_train import load_data, preprocess
 
-X_train, X_test, y_train, y_test = preprocess(load_data())
 
-pipeline = Pipeline(
-    [
-        ("scaler", StandardScaler()),
-        ("model", SVC()),
-    ]
-)
+def main() -> None:
+    X_train, X_test, y_train, y_test = preprocess(load_data())
 
-pipeline.fit(X_train, y_train)
+    pipeline = Pipeline(
+        [
+            ("scaler", StandardScaler()),
+            ("model", SVC()),
+        ]
+    )
 
-print(f"train accuracy: {pipeline.score(X_train, y_train)}")
-print(f"test accuracy: {pipeline.score(X_test, y_test)}")
+    pipeline.fit(X_train, y_train)
+
+    print(f"train accuracy: {pipeline.score(X_train, y_train)}")
+    print(f"test accuracy: {pipeline.score(X_test, y_test)}")
+
+
+if __name__ == "__main__":
+    main()
