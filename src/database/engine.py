92afb38a110146a0effe647612b60ab2b2eb61e8
Minki Kim <mingi3314@gmail.com>
2023-01-23 17:05:05 +0900

    fix: map port dynamically

--- src/database/engine.py
+++ src/database/engine.py
@@ -1,9 +1,11 @@
 import sqlalchemy as sa
 from sqlalchemy.engine.base import Engine
-
 from src.config import DB_URL
 
 
 def create_engine(host: str = None) -> Engine:
-    db_url = DB_URL.replace("localhost", host) if host else DB_URL
+    if host:
+        db_url = DB_URL.replace("localhost", host).replace("port", "5432")
+    else:
+        db_url = DB_URL.replace("port", "5433")
     return sa.create_engine(db_url, echo=True)
