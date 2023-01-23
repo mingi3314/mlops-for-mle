aefb33d4f6f61c251d9d09073b6d4c6a62a2e79d
mingi3314 <68267535+mingi3314@users.noreply.github.com>
2023-01-23 17:07:02 +0900

    feat: create saving model to registry script (#10)
    * fix: modify duplicated port
    
    * refactor: refactor pipeline training
    
    * build: remove isort
    
    * fix: map port dynamically
    
    * feat: create saving model to registry script

--- src/model_development/pipeline_train.py
+++ src/model_development/pipeline_train.py
@@ -1,12 +1,11 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-
 from src.model_development.base_train import load_data, preprocess
 
 
-def main() -> None:
-    X_train, X_test, y_train, y_test = preprocess(load_data())
+def pipeline_train(host: str = None, verbose: bool = False) -> Pipeline:
+    X_train, X_test, y_train, y_test = preprocess(load_data(host))
 
     pipeline = Pipeline(
         [
@@ -17,9 +16,12 @@ def main() -> None:
 
     pipeline.fit(X_train, y_train)
 
-    print(f"train accuracy: {pipeline.score(X_train, y_train)}")
-    print(f"test accuracy: {pipeline.score(X_test, y_test)}")
+    if verbose:
+        print(f"train accuracy: {pipeline.score(X_train, y_train)}")
+        print(f"test accuracy: {pipeline.score(X_test, y_test)}")
+
+    return pipeline
 
 
 if __name__ == "__main__":
-    main()
+    pipeline_train()
---
+++ src/model_registry/save_model_to_registry.py
@@ -0,0 +1,47 @@
+import os
+
+import click
+import mlflow
+import mlflow.sklearn
+from mlflow.models.signature import infer_signature
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from src.model_development.base_train import load_data, preprocess
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5001"
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
+
+
+@click.command()
+@click.option("--model-name", default="iris_svc", help="Name of the model", type=click.STRING)
+def main(model_name: str) -> None:
+    X_train, X_test, y_train, y_test = preprocess(load_data())
+
+    pipeline = Pipeline(
+        [
+            ("scaler", StandardScaler()),
+            ("model", SVC()),
+        ]
+    )
+
+    pipeline.fit(X_train, y_train)
+
+    signature = infer_signature(X_train, pipeline.predict(X_train))
+    input_example = X_train.iloc[:5, :]
+
+    with mlflow.start_run():
+        mlflow.log_metric("train_accuracy", pipeline.score(X_train, y_train))
+        mlflow.log_metric("test_accuracy", pipeline.score(X_test, y_test))
+        mlflow.sklearn.log_model(
+            sk_model=pipeline,
+            artifact_path=model_name,
+            signature=signature,
+            input_example=input_example,
+        )
+
+
+if __name__ == "__main__":
+    main()
